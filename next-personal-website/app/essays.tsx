00e5cc06e9f06fd6f7b70916ca8f8c146252f0aa
Haha047-UU <154788815+Haha047-UU@users.noreply.github.com>
2024-05-27 01:52:39 +0800

    Delete next-personal-website directory

--- next-personal-website/app/essays.tsx
+++
@@ -1,3 +0,0 @@
-export default function Essays() {
-  return <div>Essays</div>
-}
--- next-personal-website/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import projects from "./projects.jsx"
-import essays from "./essays.jsx"
-
-export default function Home() {
-  return <div> about me </div>
-}
